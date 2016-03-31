f3252113dbf703193eaf9ed6cfff5e06fc007529
David Kroodsma <davidkroodsma@gmail.com>
2016-03-31 09:03:22 -0700

    added EU name normalize UDF

--- clav_matching/name_normalize.js
+++ clav_matching/name_normalize.js
@@ -69,7 +69,7 @@ desired_vessel_names = ["ORYONG715","AURORAB","LUQUINGYUANYU26","OYANG355","XINS
 desired_display_names = ["ORYONG 715","AURORA B","LUQINGYUANYU 26","OYANG 355","XINSHIJI 73","GREEN TUNA 1","CAPT VINCENT GANN","KOOS 102","ORYONG 311","OCEAN WILD 2","GOLFE DU LION 5","GOLFE DU LYON 6","BAIAJI 3","AGUNG MAKMUR 27","BINTANG BATAVIA 1","","","COSTA DE SAO JORGE","HEMISFERIO NORTE","DONG BANN CHYOU 28","PASSARINHO","XIN SHI JI 67","","DANIEL DAVE 8","EL NINO","GABRIELLE L T","808","DONA BLANCA 1","NINA ZAIRA","CHIA YU 3","ALBACORA 14","KOTOSHIRO MARU 8","CINAR BALIKCILIK","KIYAK KARDESLER 3","GECICILER 4"];
 
 for(var i = 0; i<vessels.length;i++){
-	console.log(vessels[i]+"\t"+nomralizeName(vessels[i]));
+	console.log(vessels[i]+"\t"+normalizeName(vessels[i]));
 }
 
 
@@ -253,6 +253,110 @@ bigquery.defineFunction(
 );
 
 
-///// Registry_matching_sources.CLAV_12_14_2015
+///// Normalize EU query
+
+
+function normalize_EU(row, emit) {
+  var shipname_norm = row.shipname;
+  if(shipname_norm){
+    shipname_norm =  normalizeName(shipname_norm);
+  }
+  
+  emit({
+    row_number:row.row_number,
+    Country_Code:row.Country_Code,
+    CFR:row.CFR,
+    Event_Code:row.Event_Code,
+    Event_Start_Date:row.Event_Start_Date,
+    Event_End__ate:row.Event_End__ate,
+    License_Ind:row.License_Ind,
+    Registration_Nbr:row.Registration_Nbr,
+    Ext_Marking:row.Ext_Marking,
+    shipname:row.shipname,
+    shipname_norm:shipname_norm,
+    Port_Code:row.Port_Code,
+    Port_Name:row.Port_Name,
+    callsign:row.callsign,
+    IRCS:row.IRCS,
+    Vms_Code:row.Vms_Code,
+    Gear_Main_Code:row.Gear_Main_Code,
+    Gear_Sec_Code:row.Gear_Sec_Code,
+    Loa:row.Loa,
+    Lbp:row.Lbp,
+    Ton_Ref:row.Ton_Ref,
+    Ton_Gt:row.Ton_Gt,
+    Ton_Oth:row.Ton_Oth,
+    Ton_Gts:row.Ton_Gts,
+    Power_Main:row.Power_Main,
+    Power_Aux:row.Power_Aux,
+    Hull_Material:row.Hull_Material,
+    Com_Year:row.Com_Year,
+    Com_Month:row.Com_Month,
+    Com_Day:row.Com_Day,
+    Segment:row.Segment,
+    Exp_Country:row.Exp_Country,
+    Exp_Type:row.Exp_Type,
+    Public_Aid_Code:row.Public_Aid_Code,
+    Decision_Date:row.Decision_Date,
+    Decision_Seg_Code:row.Decision_Seg_Code,
+    Construction_Year:row.Construction_Year,
+    Construction_Place:row.Construction_Place
+  });
+}
+
+
+
+bigquery.defineFunction(
+  'normalize_EU',  // Name of the function exported to SQL
+  
+  ['row_number','Country_Code','CFR','Event_Code','Event_Start_Date','Event_End__ate','License_Ind',
+   'Registration_Nbr','Ext_Marking','shipname','Port_Code','Port_Name','callsign','IRCS','Vms_Code',
+   'Gear_Main_Code','Gear_Sec_Code','Loa','Lbp','Ton_Ref','Ton_Gt','Ton_Oth','Ton_Gts','Power_Main','Power_Aux','Hull_Material',
+   'Com_Year','Com_Month','Com_Day','Segment','Exp_Country','Exp_Type','Public_Aid_Code','Decision_Date',
+   'Decision_Seg_Code','Construction_Year','Construction_Place'],  // Names of input columns
+  
+  [{'name':'row_number','type':'integer'},
+  {'name':'Country_Code','type':'string'},
+  {'name':'CFR','type':'string'},
+  {'name':'Event_Code','type':'string'},
+  {'name':'Event_Start_Date','type':'string'},
+  {'name':'Event_End__ate','type':'string'},
+  {'name':'License_Ind','type':'string'},
+  {'name':'Registration_Nbr','type':'string'},
+  {'name':'Ext_Marking','type':'string'},
+  {'name':'shipname','type':'string'},
+  {'name':'shipname_norm','type':'string'},
+  {'name':'Port_Code','type':'string'},
+  {'name':'Port_Name','type':'string'},
+  {'name':'callsign','type':'string'},
+  {'name':'IRCS','type':'string'},
+  {'name':'Vms_Code','type':'string'},
+  {'name':'Gear_Main_Code','type':'string'},
+  {'name':'Gear_Sec_Code','type':'string'},
+  {'name':'Loa','type':'float'},
+  {'name':'Lbp','type':'float'},
+  {'name':'Ton_Ref','type':'float'},
+  {'name':'Ton_Gt','type':'float'},
+  {'name':'Ton_Oth','type':'float'},
+  {'name':'Ton_Gts','type':'float'},
+  {'name':'Power_Main','type':'float'},
+  {'name':'Power_Aux','type':'float'},
+  {'name':'Hull_Material','type':'string'},
+  {'name':'Com_Year','type':'string'},
+  {'name':'Com_Month','type':'string'},
+  {'name':'Com_Day','type':'string'},
+  {'name':'Segment','type':'string'},
+  {'name':'Exp_Country','type':'string'},
+  {'name':'Exp_Type','type':'string'},
+  {'name':'Public_Aid_Code','type':'string'},
+  {'name':'Decision_Date','type':'string'},
+  {'name':'Decision_Seg_Code','type':'string'},
+  {'name':'Construction_Year','type':'string'},
+  {'name':'Construction_Place','type':'string'}],
+  
+  normalize_EU                       // Reference to JavaScript UDF
+);
+
+
 
 
